5f56d2e75421767234d1bad66bb6cf246e69c5d9
Gabriel Gerez <gabrielgerez@stallo-2.local>
2018-11-05 15:48:07 +0100

    add eval_epsilon function to Cavity

--- src/chemistry/Cavity.cpp
+++ src/chemistry/Cavity.cpp
@@ -12,17 +12,47 @@ Cavity::Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, 
 }
 
 
+Cavity::Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope, double eps_i, double eps_o){
+  this->pos = coord;
+  this->R = R;
+  this->d = slope;
+  this->e_i = e_i;
+  this->e_o = e_o;
+
+}
+
+void Cavity::eval_epsilon(bool argument, bool implement){
+  this->b = argument;
+  this->is_linear = implement;
+}
+
+
 double Cavity::evalf(const double *r) const {
   double C = 1.0;
   double s, O;  
-
+ 
   for(int i = 0; i < pos.size(); i++){
     s = std::sqrt(std::pow(pos[i][0] - r[0], 2) + std::pow(pos[i][1] - r[1], 2) + std::pow(pos[i][2] - r[2], 2)) - R[i];
     O = 0.5 * (1 + std::erf(s/d));
     C *= 1 - (1 - O);
   }
   C = 1 - C;
-  return C;
+
+  if(b == false){
+    return C;
+
+  }else if(b == true){
+
+    if(is_linear == true){
+      return 1/(e_o + C*(e_i - e_o));
+
+    }else{
+      return (1/e_i)*std::exp(log(e_i/e_o)*(1 - C));
+
+    }
+     
+
+  }
 }
 
 
--- src/chemistry/Cavity.h
+++ src/chemistry/Cavity.h
@@ -10,6 +10,8 @@ namespace mrchem {
 class Cavity : public mrcpp::RepresentableFunction<3> {
 public: 
   Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope);
+  Cavity(std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope, double e_i, double e_o);
+  void eval_epsilon(bool argument, bool implement);
   double evalf(const double *r) const override; 
   double evalf(const std::array<double, 3> &r) const {return evalf(r.data());};
 
@@ -17,6 +19,10 @@ protected:
   std::vector<std::array<double, 3>> pos;
   std::vector<double> R;
   double d;
+  double e_i = 1;
+  double e_o = 2;
+  bool b = false;
+  bool is_linear = false;
 };
 
 }
