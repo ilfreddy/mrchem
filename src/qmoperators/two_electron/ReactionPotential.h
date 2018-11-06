6c068cf9a026ea8f5959e3d6220ce2431618a677
Gabriel Gerez <gabrielgerez@stallo-1.local>
2018-11-06 16:27:35 +0100

    Edit Reactionpotential

--- src/qmoperators/two_electron/ReactionPotential.h
+++ src/qmoperators/two_electron/ReactionPotential.h
@@ -8,29 +8,15 @@ namespace mrchem{
 
 class ReactionPotential{
 public:
-  ReactionPotential(const FunctionTree<3> &rho_tree, std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope); //initialize in default
-  ReactionPotential(const FunctionTree<3> &rho_tree, std::vector<std::array<double, 3>> coord, std::vector<double> R, double slope, double e_i, double e_o, bool is_lin);
+  ReactionPotential(const FunctionTree<3> &rho_tree, FunctionTree<3> cavity_tree, FunctionTree<3> epsilon_tree); //initialize in default
   void rho_eff();
   void gamma();
-
+  
 protected:
+  FunctionTree<3> rho_tree, cavity_tree, epsilon_tree, rho_eff_tree, gamma_tree;
   bool is_lin = false;
-  Cavity C;
-  Cavity eps_inv;
-  FunctionTree<3> rho_tree;
-  FunctionTree<3> rho_eff_tree;
-  FunctionTree<3> gamma_tree;
-
-  double prec = 1.0e-6;
-  int n = -4;
-  int l[3] = {-1, -1, -1};
-  int nb[3] = {2, 2, 2};
-  int N = 25;
-  int k = 7;
-
-  BoundingBox<3> world(n, l, nb);
-  ScalingBasis basis =  InterpolatingBasis(k);
-  MultiResolutionAnalysis<3> MRA(world, basis, N);
+  
+  
 
 
 };
