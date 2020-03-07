5a55205aede7aad75a524d7394d8f0ec33811b9f
Luca Frediani <luca.frediani@uit.no>
2020-03-07 10:54:29 +0100

    Add Zora kinetic energy operator

---
+++ src/qmoperators/one_electron/ZoraPotential.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "analyticfunctions/NuclearFunction.h"
+#include "qmoperators/RankZeroTensorOperator.h"
+#include "qmoperators/one_electron/QMPotential.h"
+
+namespace mrchem {
+
+class ZoraPotential final : public QMPotential {
+public:
+    ZoraPotential(const Nuclei &nucs, double proj_prec, double smooth_prec = -1.0, bool mpi_share = false);
+    ~ZoraPotential() override { free(NUMBER::Total); }
+
+    void setup(double prec) override { setApplyPrec(prec); }
+    void clear() override { clearApplyPrec(); }
+
+private:
+    NuclearFunction func;
+
+    void allreducePotential(double prec, QMFunction &V_loc);
+};
+
+} // namespace mrchem
