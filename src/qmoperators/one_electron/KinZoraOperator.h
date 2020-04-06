cd680e965dcbd334b7aad78dfc4386f71207550c
Luca Frediani <luca.frediani@gmail.com>
2020-04-06 11:38:05 +0200

    Adds ZORA kinetic energy

---
+++ src/qmoperators/one_electron/KinZoraOperator.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "MomentumOperator.h"
+#include "ZoraOperator.h"
+#include "qmoperators/RankZeroTensorOperator.h"
+
+/** @class KineticOperator
+ *
+ * @brief Operator for kinetic energy
+ *
+ * This operator is constructed as the square of the more fundamental
+ * MomentumOperator. The general base class functions for calculation of
+ * expectation values are overwritten, as they can be improved due to
+ * symmetry.
+ *
+ */
+
+namespace mrchem {
+
+class KinZoraOperator final : public RankZeroTensorOperator {
+public:
+    KinZoraOperator(std::shared_ptr<mrcpp::DerivativeOperator<3>> D,
+		    const Nuclei &nucs,
+		    double proj_prec,
+		    double smooth_prec = -1.0,
+		    bool mpi_share = false)
+            : p(D)
+	    , vz(nucs, proj_prec, smooth_prec, mpi_share, true) {
+        // Invoke operator= to assign *this operator
+        RankZeroTensorOperator &t = (*this);
+        t = 0.5 * (p[0] * vz * p[0] + p[1] * vz * p[1] + p[2] * vz * p[2]);
+        t.name() = "T";
+    }
+
+    ComplexMatrix operator()(OrbitalVector &bra, OrbitalVector &ket);
+    ComplexMatrix dagger(OrbitalVector &bra, OrbitalVector &ket);
+
+    using RankZeroTensorOperator::operator();
+    using RankZeroTensorOperator::dagger;
+
+private:
+    MomentumOperator p;
+    ZoraOperator vz;
+};
+
+} // namespace mrchem
---
+++ src/qmoperators/one_electron/ZoraOperator.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "analyticfunctions/NuclearFunction.h"
+#include "qmoperators/RankZeroTensorOperator.h"
+#include "qmoperators/one_electron/QMPotential.h"
+#include "qmoperators/one_electron/ZoraPotential.h"
+
+namespace mrchem {
+
+class ZoraOperator final : public RankZeroTensorOperator {
+public:
+    ZoraOperator(const Nuclei &nucs,
+		 double proj_prec,
+		 double smooth_prec = -1.0,
+		 bool mpi_share = false,
+		 bool inverse = false) {
+        r_m1 = std::make_shared<ZoraPotential>(nucs, proj_prec, smooth_prec, mpi_share, inverse);
+        // Invoke operator= to assign *this operator
+        RankZeroTensorOperator &v = (*this);
+        v = r_m1;
+        v.name() = "V_nuc";
+    }
+
+private:
+    std::shared_ptr<ZoraPotential> r_m1{nullptr};
+};
+
+} // namespace mrchem
