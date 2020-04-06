88aad24020400fcdbd2606a31445cb174ccda354
Luca Frediani <luca.frediani@gmail.com>
2020-04-06 11:06:52 +0200

    Add bool to choose either 1-V/c^2 (default) and kappa

--- src/qmoperators/one_electron/ZoraPotential.h
+++ src/qmoperators/one_electron/ZoraPotential.h
@@ -8,15 +8,19 @@ namespace mrchem {
 
 class ZoraPotential final : public QMPotential {
 public:
-    ZoraPotential(const Nuclei &nucs, double proj_prec, double smooth_prec = -1.0, bool mpi_share = false);
+    ZoraPotential(const Nuclei &nucs,
+                  double proj_prec,
+                  double smooth_prec = -1.0,
+                  bool mpi_share = false,
+                  bool inverse = false);
     ~ZoraPotential() override { free(NUMBER::Total); }
 
     void setup(double prec) override { setApplyPrec(prec); }
     void clear() override { clearApplyPrec(); }
-    void computeZora(double prec = -1.0);
-    void computeKappa(double prec = -1.0);
 
 private:
+    void computeZora(double prec = -1.0);
+    void computeKappa(double prec = -1.0);
     NuclearFunction func;
     void allreducePotential(double prec, QMFunction &V_loc);
 };
