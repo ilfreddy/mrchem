4dde4502f2cb1e8201a19b4cb78514054aaeaf35
Luca Frediani <luca.frediani@gmail.com>
2020-04-17 09:38:48 +0200

    ZORA Kinetic energy used in Fock operator

--- src/qmoperators/two_electron/FockOperator.cpp
+++ src/qmoperators/two_electron/FockOperator.cpp
@@ -62,7 +62,7 @@ FockOperator::FockOperator(KineticOperator_p t,
 void FockOperator::build(double exx) {
     this->exact_exchange = exx;
     this->T = RankZeroTensorOperator();
-    if (this->kin != nullptr) this->T += (*this->kin);
+    if (this->zora != nullptr) this->T += (*this->zora);
 
     this->V = RankZeroTensorOperator();
     if (this->nuc != nullptr) this->V += (*this->nuc);
@@ -149,7 +149,7 @@ SCFEnergy FockOperator::trace(OrbitalVector &Phi, const Nuclei &nucs) {
     if (this->ext != nullptr) E_next = -this->ext->trace(nucs).real();
 
     // Electronic part
-    if (this->kin != nullptr) E_kin = this->kin->trace(Phi).real();
+    if (this->zora != nullptr) E_kin = this->zora->trace(Phi).real();
     if (this->nuc != nullptr) E_en = this->nuc->trace(Phi).real();
     if (this->coul != nullptr) E_ee = 0.5 * this->coul->trace(Phi).real();
     if (this->ex != nullptr) E_x = -this->exact_exchange * this->ex->trace(Phi).real();
@@ -166,7 +166,7 @@ ComplexMatrix FockOperator::operator()(OrbitalVector &bra, OrbitalVector &ket) {
     auto plevel = Printer::getPrintLevel();
     mrcpp::print::header(2, "Computing Fock matrix");
 
-    auto t = this->getKineticOperator();
+    auto t = this->getKinZoraOperator();
     auto v = this->potential();
 
     ComplexMatrix T = ComplexMatrix::Zero(bra.size(), ket.size());
