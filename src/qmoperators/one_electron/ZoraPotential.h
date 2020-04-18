3783b531f78da97df55964ea3a189c4cf1c19fbf
Luca Frediani <luca.frediani@gmail.com>
2020-04-18 10:13:26 +0200

    Add VZora and LnZeta to ZoraPotential

--- src/qmoperators/one_electron/ZoraPotential.h
+++ src/qmoperators/one_electron/ZoraPotential.h
@@ -12,15 +12,17 @@ public:
                   double proj_prec,
                   double smooth_prec = -1.0,
                   bool mpi_share = false,
-                  bool inverse = false);
+                  int func_flag = 0);
     ~ZoraPotential() override { free(NUMBER::Total); }
 
     void setup(double prec) override { setApplyPrec(prec); }
     void clear() override { clearApplyPrec(); }
 
 private:
+    void computeVZora(double prec = -1.0);
     void computeZora(double prec = -1.0);
     void computeKappa(double prec = -1.0);
+    void computeLnKappa(double prec = -1.0);
     NuclearFunction func;
     void allreducePotential(double prec, QMFunction &V_loc);
 };
