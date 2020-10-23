5a352e0064806e99dba449cb81b8fb70454c5be0
Luca Frediani <luca.frediani@gmail.com>
2020-10-23 14:21:18 +0200

    Silent ZORA does not break any tests

--- src/qmoperators/two_electron/FockOperator.cpp
+++ src/qmoperators/two_electron/FockOperator.cpp
@@ -66,7 +66,8 @@ FockOperator::FockOperator(KineticOperator_p t,
 void FockOperator::build(double exx) {
     this->exact_exchange = exx;
     this->T = RankZeroTensorOperator();
-    if (this->zora != nullptr) this->T += (*this->zora);
+    //      if (this->zora != nullptr) this->T += (*this->zora);
+    if (this->kin != nullptr) this->T += (*this->kin);
 
     this->V = RankZeroTensorOperator();
     if (this->nuc != nullptr) this->V += (*this->nuc);
@@ -93,7 +94,7 @@ void FockOperator::setup(double prec) {
     mrcpp::print::header(2, "Building Fock operator");
     mrcpp::print::value(2, "Precision", prec, "(rel)", 5);
     mrcpp::print::separator(2, '-');
-    this->zora->setup(prec);
+    //    this->zora->setup(prec);
     this->kinetic().setup(prec);
     this->potential().setup(prec);
     this->perturbation().setup(prec);
@@ -111,7 +112,7 @@ void FockOperator::clear() {
     this->kinetic().clear();
     this->potential().clear();
     this->perturbation().clear();
-    this->zora->clear();
+    //    this->zora->clear();
 }
 
 /** @brief rotate orbitals of two-electron operators
@@ -163,7 +164,8 @@ SCFEnergy FockOperator::trace(OrbitalVector &Phi, const Nuclei &nucs) {
         Er_el = 0.5 * this->Ro->getElectronicEnergy();
     }
     // Electronic part
-    if (this->zora != nullptr) E_kin = this->zora->trace(Phi).real();
+    //    if (this->zora != nullptr) E_kin = this->zora->trace(Phi).real();
+    if (this->kin != nullptr) E_kin = this->kin->trace(Phi).real();
     if (this->nuc != nullptr) E_en = this->nuc->trace(Phi).real();
     if (this->coul != nullptr) E_ee = 0.5 * this->coul->trace(Phi).real();
     if (this->ex != nullptr) E_x = -this->exact_exchange * this->ex->trace(Phi).real();
@@ -180,7 +182,8 @@ ComplexMatrix FockOperator::operator()(OrbitalVector &bra, OrbitalVector &ket) {
     auto plevel = Printer::getPrintLevel();
     mrcpp::print::header(2, "Computing Fock matrix");
 
-    auto t = this->getKinZoraOperator();
+    //    auto t = this->getKinZoraOperator();
+    auto t = this->getKineticOperator();
     auto v = this->potential();
 
     ComplexMatrix T = ComplexMatrix::Zero(bra.size(), ket.size());
